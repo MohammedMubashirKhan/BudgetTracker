aa56ec9b6286c207b9e183f85447e04104e9f5fd
unknown <mubashir.khan5059@gmail.com>
2023-11-23 23:19:10 +0530

    bg color is put and goal heading is put

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,51 +8,28 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text("Flutter Demo Home Page"),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Obx(
-              () => Text(
-                '${controller.count}',
-                style: Theme.of(context).textTheme.headlineMedium,
+      backgroundColor: const Color.fromRGBO(213, 228, 254, 1),
+      body: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 20.0),
+          decoration: const BoxDecoration(
+            color: Color.fromRGBO(46, 46, 124, 1),
+          ),
+          child: const Column(
+            children: <Widget>[
+              // Goal title
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Buy a dream house',
+                      style: TextStyle(color: Colors.white, fontSize: 40.0)),
+                ],
               ),
-            ),
-          ],
+              // Goal Progress
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
