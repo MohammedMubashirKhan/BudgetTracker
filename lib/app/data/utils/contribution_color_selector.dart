8759793bb77d9ac3ed251ccbc2a8fc326a4085c9
unknown <mubashir.khan5059@gmail.com>
2023-11-27 00:59:12 +0530

    new color is add in contributionColor

--- lib/app/data/utils/contribution_color_selector.dart
+++ lib/app/data/utils/contribution_color_selector.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 Color contributionColor(int index) {
   // Assign a different color to each contribution based on its index
-  List<Color> colors = [Colors.red, Colors.green, Colors.blue, Colors.purple];
+  List<Color> colors = [
+    Colors.cyan,
+    Colors.green,
+    Colors.blue,
+    Colors.pinkAccent
+  ];
   return colors[index % colors.length];
 }
